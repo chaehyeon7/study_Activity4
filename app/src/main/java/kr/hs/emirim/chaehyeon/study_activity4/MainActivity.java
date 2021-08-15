01c21223c9b7249481a8bb58365e152f835e4d65
chaehyeon7 <81363193+chaehyeon7@users.noreply.github.com>
2021-08-15 13:59:22 +0900

    Edit 클릭_ 터치 등 이벤트 처리(MainActivity.java 메서드 추가) Commit

--- app/src/main/java/kr/hs/emirim/chaehyeon/study_activity4/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/chaehyeon/study_activity4/MainActivity.java
@@ -2,13 +2,42 @@ package kr.hs.emirim.chaehyeon.study_activity4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_blue, btn_red;
+    TextView txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        //사용할 객체들을 검색
+        btn_blue = findViewById(R.id.btn_blue);
+        btn_red = findViewById(R.id.btn_red);
+        txt = findViewById(R.id.txt);
+
+        //버튼에 이벤트 감지자 등록
+        btn_blue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //버튼을 클릭 했을때 이벤트 처리 영역
+                txt.setBackgroundColor(Color.BLUE);
+            }
+        });
+
+        btn_red.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txt.setBackgroundColor(Color.RED);
+            }
+        });
+
+    }//onCreate()
 }
